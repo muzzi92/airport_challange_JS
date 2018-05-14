14ddb779ad3ea214199fd53754fc60284bf532fd
Igor Ryabchuk <riabtchuk@gmail.com>
2018-05-14 17:30:23 +0100

    adding spec for airport methods

--- jasmine/spec/airportChallengeSpec.js
+++ jasmine/spec/airportChallengeSpec.js
@@ -3,7 +3,22 @@ describe('airport', function(){
   describe('can land planes', function(){
     it('should have array for planes', function(){
       airport = new Airport();
-      expect(airport.planes).toBe([]);
+      expect(airport.planes).toEqual([]);
     });
+
+    it('should have plane in array after landing', function () {
+      airport = new Airport();
+      var plane = 'plane';
+      airport.land(plane);
+      expect(airport.planes).toEqual([plane]);
+    });
+
+    it('should remove the plane from the array after takeoff', function () {
+      airport = new Airport();
+      var plane = 'plane';
+      airport.takeOff(plane);
+      expect(airport.planes).toEqual([]);
+    })
+
   });
 });
--- jasmine/src/airportChallenge.js
+++ jasmine/src/airportChallenge.js
@@ -1,3 +1,11 @@
 function Airport() {
   this.planes = [];
+}
+
+Airport.prototype.land = function (plane) {
+  this.planes.push(plane);
+};
+
+Airport.prototype.takeOff = function (plane) {
+  this.planes.splice(this.planes.indexOf(plane), 1);
 };
